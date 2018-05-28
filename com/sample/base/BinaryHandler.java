bf326622313fd3ba2ff5c91c7c4c5ed9e9e6df52
richard <xinsenlu@github.com>
2018-05-28 16:32:45 +0800

    parser baseline implemented

---
+++ com/sample/base/BinaryHandler.java
@@ -0,0 +1,181 @@
+package com.sample.base;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class BinaryHandler {
+	public static String parseFile(String args_1,String args_2) throws XPathExpressionException {
+		try {
+		File input=new File("validate.bin");
+		FileInputStream fis=new FileInputStream(input);
+		File xml= new File("validate.xml");
+		long length=input.length();
+		byte[] bytes = new byte[(int) length];
+		fis.read(bytes);
+		fis.close();
+//		XPATH
+        FileInputStream fileIS = new FileInputStream(xml);
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = builderFactory.newDocumentBuilder();
+        Document xmlDocument = builder.parse(fileIS);
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        String expression_name = "/atfx_file/instance_data/MeaQuantity/Name/text()";
+        String expression_unit="/atfx_file/instance_data/MeaQuantity/Unit/text()";
+        String expression_datatype="/atfx_file/instance_data/MeaQuantity/DataType/text()";
+        String unit_name="/atfx_file/instance_data/Unit/Name/text()";
+        String external_blocksize="/atfx_file/instance_data/ExternalComponent/Blocksize/text()";
+        String external_valuesperblock="/atfx_file/instance_data/ExternalComponent/ValuesPerBlock/text()";
+        String external_valueoffset="/atfx_file/instance_data/ExternalComponent/ValueOffset/text()";
+        String external_length="/atfx_file/instance_data/ExternalComponent/Length/text()";
+        String external_startoffset="/atfx_file/instance_data/ExternalComponent/StartOffset/text()";
+
+        NodeList nodeList_name = (NodeList) xPath.compile(expression_name).evaluate(xmlDocument, XPathConstants.NODESET);
+        NodeList nodeList_unit = (NodeList) xPath.compile(expression_unit).evaluate(xmlDocument, XPathConstants.NODESET);
+        NodeList nodeList_unit_name=(NodeList) xPath.compile(unit_name).evaluate(xmlDocument, XPathConstants.NODESET);
+        NodeList nodeList_datatype = (NodeList) xPath.compile(expression_datatype).evaluate(xmlDocument, XPathConstants.NODESET);
+        NodeList nodeList_external_blocksize=(NodeList) xPath.compile(external_blocksize).evaluate(xmlDocument, XPathConstants.NODESET);
+        NodeList nodeList_external_valuesperblock=(NodeList) xPath.compile(external_valuesperblock).evaluate(xmlDocument, XPathConstants.NODESET);
+        NodeList nodeList_external_valueoffset=(NodeList) xPath.compile(external_valueoffset).evaluate(xmlDocument, XPathConstants.NODESET);
+        NodeList nodeList_external_length=(NodeList) xPath.compile(external_length).evaluate(xmlDocument, XPathConstants.NODESET);
+        NodeList nodeList_external_startoffset=(NodeList) xPath.compile(external_startoffset).evaluate(xmlDocument, XPathConstants.NODESET);
+
+        int len=nodeList_name.getLength();
+		File file= new File("test_1.csv");
+		 PrintWriter pw = new PrintWriter(file);
+		 StringBuilder sb = new StringBuilder();
+		//Name
+		 	for (int i=0;i<len;i++) {
+		 		sb.append(nodeList_name.item(i).getTextContent());
+		 		if(i==len-1) {
+		 			sb.append('\n');
+		 		}
+		 		else sb.append(',');
+		 	}
+	    //Unit
+		 	for(int i=0;i<len;i++) {
+		 		int index=Integer.valueOf(nodeList_unit.item(i).getTextContent());
+		 		sb.append(nodeList_unit_name.item(index-1).getTextContent());
+		 		if(i==len-1) {
+		 			sb.append('\n');
+		 		}
+		 		else sb.append(',');
+		 	
+		 	}
+	 	//int StartOffset
+	     //int Blocksize
+		 //int ValuesPerBlock
+		 //int ValueOffset
+	 	 //int Length
+	 	 //String DataType
+		
+		//value 
+		 	for (int j=0;j<getMaxOfList(nodeList_external_length);j++) {
+		 		for(int i=0;i<len;i++)
+		 		{
+		 			int outerOffset=Integer.valueOf(nodeList_external_startoffset.item(i).getTextContent());
+		 			int innerOffset=Integer.valueOf(nodeList_external_valueoffset.item(i).getTextContent());
+		 			int step=Integer.valueOf(nodeList_external_blocksize.item(i).getTextContent());
+		 			switch(nodeList_datatype.item(i).getTextContent()) {
+		 			case "DT_FLOAT":
+		 				int id1=get32(bytes,j,outerOffset,innerOffset,step);
+		 				float id_1=Float.intBitsToFloat(id1);
+		 				sb.append(id_1);
+		 				break;
+		 			case "DT_LONG":
+		 				long id_2=(long)get32(bytes,j,outerOffset,innerOffset,step);
+		 				sb.append(id_2);
+		 				break;
+		 			case "DT_SHORT":
+		 				short id_3=get16(bytes,j,outerOffset,innerOffset,step);
+		 				sb.append(id_3);
+		 				break;
+		 			case "DT_DOUBLE":
+		 				long id4=get64(bytes,j,outerOffset,innerOffset,step);
+		 				double id_4=Double.longBitsToDouble(id4);
+		 				sb.append(id_4);
+		 				break;
+	 				default:
+	 					break;
+		 			}
+		 			if(i==len-1) {
+			 			sb.append('\n');
+			 		}
+			 		else sb.append(',');
+		 		}
+		 	}
+		
+		
+	        pw.print(sb.toString());
+	        pw.close();
+	        
+	        
+	        
+		
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "test_1.csv";	
+		
+		
+	}
+	
+   protected static long get64 (byte[] bytes,int i ,int outerOffset,int innerOffset,int step) {
+	   long value = 0;
+		value += (long) (bytes[i*step+7+innerOffset+outerOffset] & 0x000000FF) << 56;
+		value += (long) (bytes[i*step+6+innerOffset+outerOffset] & 0x000000FF) << 48;
+		value += (long) (bytes[i*step+5+innerOffset+outerOffset] & 0x000000FF) << 40;
+		value += (long) (bytes[i*step+4+innerOffset+outerOffset] & 0x000000FF) << 32;
+		value += (bytes[i*step+3+innerOffset+outerOffset] & 0x000000FF) << 24;
+		value += (bytes[i*step+2+innerOffset+outerOffset] & 0x000000FF) << 16;
+		value += (bytes[i*step+1+innerOffset+outerOffset] & 0x000000FF) << 8;
+		value += (bytes[i*step+innerOffset+outerOffset] & 0x000000FF);
+	   return value;
+	}
+   protected static int get32 (byte[] bytes,int i,int outerOffset,int innerOffset,int step) {
+	   int value = 0;
+	   value += (bytes[i*step+3+innerOffset+outerOffset] & 0x00FF) << 24;
+		value += (bytes[i*step+2+innerOffset+outerOffset] & 0x00FF) << 16;
+		value += (bytes[i*step+1+innerOffset+outerOffset] & 0x00FF) << 8;
+		value += (bytes[i*step+innerOffset+outerOffset] & 0x00FF);
+	   return value;
+	}
+   protected static short get16 (byte[] bytes,int i,int outerOffset,int innerOffset,int step) {
+	   short value = 0;
+		value += (bytes[i*step+1+innerOffset+outerOffset] & 0xFF) << 8;
+		value += (bytes[i*step+innerOffset+outerOffset] & 0xFF);
+	   return value;
+	}
+   protected static int getMaxOfList (NodeList list) {
+	   int length=list.getLength();
+	   int maxValue=0;
+	   for (int i=0;i<length;i++) {
+		   if(Integer.valueOf(list.item(i).getTextContent())>maxValue) 
+		   {
+			   maxValue=Integer.valueOf(list.item(i).getTextContent());
+		   }
+	   }
+	   return maxValue;
+   }
+   
+   
+}
